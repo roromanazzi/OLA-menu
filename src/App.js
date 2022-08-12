4846385455f6d83374869395e0d65fa048a126a3
Ro Romanazzi <rocioromanazzi@gmail.com>
2022-08-12 01:57:56 -0300

    First working version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sideImgSrc from "./img/langostinos.jpg";
+import mainImgSrc from "./img/ñoquis.jpg";
+import frontPageImgSrc from "./img/olaPortada.png";
+import footerImgSrc from "./img/olafooter.png";
+
+import { menuSections } from "./placeholder/menuItems";
+
+function FrontPageImg(props) {
+  return <img src={frontPageImgSrc} className="frontPageImg" alt="foto de portada"></img>;
+}
+
+function MainImg(props) {
+  return <img src={mainImgSrc} className="mainImg" alt="segunda foto"></img>;
+}
+
+function SideImg(props) {
+  return <img src={sideImgSrc} className="sideImg" alt="foto al costado"></img>;
+}
+
+function MenuSection(props) {
+  return <section className="menuSection">{props.children}</section>;
+}
+function MenuSectionTitle(props) {
+  return <h1 className="menuSectionTitle">// {props.children}</h1>;
+}
+function MenuSectionDescription(props) {
+  if (props.children) {
+    return <div className="menuSectionDescription">{props.children}</div>;
+  } else {
+    return null;
+  }
+}
+
+function MenuSectionDishes(props) {
+  return <div className="menuSectionDishes">{props.children}</div>;
+}
+
+function Dish(props) {
+  return <div className="dish">{props.children}</div>;
+}
+
+function DishHeader(props) {
+  return <div className="dishHeader">{props.children}</div>;
+}
+
+function DishTitle(props) {
+  return <h2 className="dishTitle">{props.children}</h2>;
+}
+
+function DishPortion(props) {
+  if (props.children) {
+    return <div className="dishPortion">x{props.children}</div>;
+  } else {
+    return null;
+  }
+}
+
+function DishPrice(props) {
+  return <div className="dishPrice">${props.children}</div>;
+}
+
+function DishDescription(props) {
+  return <div className="dishDescription">{props.children}</div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FrontPageImg></FrontPageImg>
+      <MainImg></MainImg>
+      <div className="page">
+        <div className="sections">
+          {menuSections.map((section) => {
+            return (
+              <MenuSection key={section.title}>
+                <MenuSectionTitle>{section.title}</MenuSectionTitle>
+                <MenuSectionDescription>{section?.description}</MenuSectionDescription>
+                <MenuSectionDishes>
+                  {section.dishes.map((dish) => {
+                    return (
+                      <Dish key={dish.title}>
+                        <DishHeader>
+                          <DishTitle>{dish.title}</DishTitle>
+                          <DishPortion>{dish?.portion}</DishPortion>
+                          <DishPrice>{dish.price}</DishPrice>
+                        </DishHeader>
+                        <DishDescription>{dish?.description}</DishDescription>
+                      </Dish>
+                    );
+                  })}
+                </MenuSectionDishes>
+              </MenuSection>
+            );
+          })}
+          <MenuSection>
+            <footer>
+              <img src={footerImgSrc} alt="logo de Ola" className="footerImg"></img>
+            </footer>
+            <footer>Coded by Rocío Romanazzi</footer>
+          </MenuSection>
+        </div>
+        <div className="sideImgContainer">
+          <SideImg></SideImg>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/placeholder/menuItems.js
@@ -0,0 +1,196 @@
+export const menuSections = [
+  {
+    title: "PLATITOS",
+    dishes: [
+      {
+        title: "TABLITA",
+        price: 2100,
+        description: "Queso gouda, brie y reggianito; jamón crudo, spianata de hierbas y pepperoni Las Dinas.",
+      },
+      {
+        title: "PAPAS OLAVA",
+        price: 1400,
+        description: "Con cheddar, pico de gallo, barbacoa y pulled pork.",
+      },
+      {
+        title: "CEVICHE",
+        price: 1100,
+        description: "De pesca del día.",
+      },
+      {
+        title: "TORTILLA ESPAÑOLA",
+        price: 1800,
+        description: "Con chorizo colorado.",
+      },
+      {
+        title: "EMPANADITAS",
+        portion: 3,
+        price: 900,
+        description: "Entraña | Caprese | Bondiola.",
+      },
+      {
+        title: "BRUSQUETAS DE SALMON",
+        portion: 2,
+        price: 1700,
+        description:
+          "Tostones de pan brioche, gravlax de salmón, queso crema con ciboulette, micro ensalada y salsa teriyaki.",
+      },
+      {
+        title: "RABAS",
+        price: 1850,
+        description: "Con limón y dip de alioli.",
+      },
+      {
+        title: "BOMBITA DARIN",
+        price: 950,
+        description: "Bombas de papa con queso y crispy bacon, con alioli de sriracha.",
+      },
+      {
+        title: "LANGOSTINOS EMPANADOS",
+        price: 1700,
+      },
+    ],
+  },
+  {
+    title: "PIZZAS",
+    description: "6 porciones - Masa madre y fermentación larga.",
+    dishes: [
+      {
+        title: "MOZZARELLA",
+        price: 1600,
+      },
+      {
+        title: "TOMATE Y ALBAHACA",
+        price: 1700,
+      },
+      {
+        title: "PEPPERONI",
+        price: 1800,
+      },
+    ],
+  },
+  {
+    title: "ENTRE PANES",
+    dishes: [
+      {
+        title: "PHILLY TOMMY",
+        price: 1700,
+        description: "Dados de lomo, cebolla caramelizada y cheddar en pan brioche acompañados de papas fritas.",
+      },
+      {
+        title: "BURGER",
+        price: 1500,
+        description:
+          "Blend de carne de 180g, queso, tomate, lechuga, cebolla morada y salsa especial acompañada de papas fritas.",
+      },
+      {
+        title: "SANDWICH DE SALMON",
+        price: 1600,
+        description: "Queso crema, ciboulette y rúcula con guarnición de hojas verdes.",
+      },
+      {
+        title: "PORTOBELLO ROAD",
+        price: 1200,
+        description: "En pan integral con semillas, tomate, zuchinni, queso y cebolla morada.",
+      },
+      {
+        title: "CLUB CHICKEN",
+        price: 1550,
+        description: "Pollo, queso, cebolla caramelizada, palta y tomates confitados.",
+      },
+    ],
+  },
+  {
+    title: "ENSALADAS",
+    dishes: [
+      {
+        title: "OLAVA",
+        price: 1650,
+        description: "Rúcula, champignones, cebolla caramelizada, cherries y salmón.",
+      },
+      {
+        title: "CAESAR",
+        price: 1700,
+        description: "Pollo, hojas verdes, parmesano, croutones y aderezo Caesar.",
+      },
+      {
+        title: "COUS COUS & LANGOSTINOS",
+        price: 1500,
+        description: "Cous cous, langostinos, verdes, cebolla morada, cherries, cremoso de zapallo, oliva y semillas.",
+      },
+    ],
+  },
+  {
+    title: "PRINCIPALES",
+    dishes: [
+      {
+        title: "RISOTTO DE HONGOS",
+        price: 1700,
+        description: "Con champignones, hongos de pino y portobellos.",
+      },
+      {
+        title: "TERNERITA AL MALBEC",
+        price: 1800,
+        description: "Ternera en cocción lenta y reducción de vino tinto con puré de papa.",
+      },
+      {
+        title: "MILA COMO EN CASA",
+        price: 2200,
+        description: "Milanesa de ojo de bife con fideos con manteca y queso gratén.",
+      },
+      {
+        title: "ESTOFADO DE CORDERO",
+        price: 1800,
+        description: "Pasta seca De Cecco con estofado de cordero en su salsa.",
+      },
+      {
+        title: "WOK",
+        price: 1550,
+        description: "De vegetales con cerdo marinado y arroz yamaní.",
+      },
+      {
+        title: "MILA NAPO",
+        price: 2350,
+        description: "Milanesa de ojo de bife napolitana con puré de papas.",
+      },
+      {
+        title: "ÑOQUIS SOUFFLE",
+        price: 1600,
+        description: "De espinaca gratinados.",
+      },
+      {
+        title: "LASAGNA DE OSOBUCO",
+        price: 1700,
+        description: "Lasagna de estofado de osobuco con su caracú.",
+      },
+      {
+        title: "PASTEL DE CORDERO",
+        price: 2000,
+        description: "En masa sablée con puré de boniato.",
+      },
+    ],
+  },
+  {
+    title: "POSTRES",
+    dishes: [
+      {
+        title: "FLAN DE PANDE",
+        price: 750,
+        description: "Flan de dulce de leche con más dulce de leche que flan.",
+      },
+      {
+        title: "VOLCAN",
+        price: 750,
+        description: "De chocolate con helado de americana y salsa de frutos rojos.",
+      },
+      {
+        title: "TIRAMISU",
+        price: 750,
+      },
+      {
+        title: "CREME BRULEE",
+        price: 750,
+      },
+    ],
+  },
+];
